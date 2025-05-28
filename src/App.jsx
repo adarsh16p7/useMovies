1ae626e93827c051897d44dbb6bd629ba319c75f
adarsh16p7 <adarsh.patel0716@gmail.com>
2025-05-28 16:10:04 +0530

    Added react router and Favorites page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
     <div className='main'>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
     </div>
   );
 }
